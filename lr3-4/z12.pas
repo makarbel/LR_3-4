3c7cbbde79c30c3ce2037b3006624a805b8f8c8e
makarbel <144238241+makarbel@users.noreply.github.com>
2023-10-10 10:23:49 +0300

    Update z12.pas

--- lr3-4/z12.pas
+++ lr3-4/z12.pas
@@ -1,22 +1,19 @@
-﻿program zad12;
-var a,b,j,P,S,i,C:integer;
+program zad12;
+var a, b, j, P, S: integer;
+begin
+  write('Введите диапазон от ');
+  read(a);
+  write('Введите диапазон до ');
+  read(b);
+  P := 0;
+  S := 1;
+  for j := a to b do
   begin
-    write('Введите диапазон от ');
-    read(a);
-    write('Введите диапазон до ');
-    read(b);
-    i:=a;
-    C:=0;
-    for j:=a to b do begin
-      if(j mod 2 = 0) then C:=C+1
-    end;
-    if(C>=1)then S:=1 else S:=0;
-    P:=0;
-    repeat
-      if (i mod 2=0) then S:=S*i
-      else P:=P+i;
-      i:=i+1;
-    until i>b;
-    writeln('Сумма нечетных: ',P);
-    writeln('Произведение четных: ',S);
-  end.
+    if j mod 2 = 0 then
+      S := S * j
+    else
+      P := P + j;
+  end;
+  writeln('Сумма нечетных: ', P);
+  writeln('Произведение четных: ', S);
+end.
